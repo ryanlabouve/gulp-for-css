985208341af0e6a43358fdf85cf89547f5d5870f
Ryan LaBouve <imryanlabouve@gmail.com>
2014-11-23 23:40:11 -0600

    Gulp connect server running

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var connect = require('gulp-connect');
 
-gulp.task('default', function() {
-    gutil.log("Running Gulp!");
+gulp.task('connect', function() {
+    connect.server({
+        port: 8989,
+        livereload: true
+    });
 });
+
+gulp.task('default', ['connect']);
